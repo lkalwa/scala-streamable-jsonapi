a737a189757d2af080255d7b2d65cb49ed941260
lkalwa <leszek.kalwa@gmail.com>
2019-04-10 16:25:28 +0200

    JsonApiStack now inherits from `ArrayStack`

--- src/main/scala/com.github.lkalwa.scala_streamable_jsonapi/JsonApiStack.scala
+++ src/main/scala/com.github.lkalwa.scala_streamable_jsonapi/JsonApiStack.scala
@@ -3,19 +3,15 @@ package com.github.lkalwa.scala_streamable_jsonapi
 import org.codehaus.jackson.JsonToken
 import scala.collection.mutable.ArrayStack
 
-class JsonApiStack {
-  private val stack = new ArrayStack[JsonToken]()
+class JsonApiStack extends ArrayStack[JsonToken] {
 
-  def pop: JsonToken = stack.pop
+  def topLevelMember: Boolean = equal(JsonToken.START_OBJECT)
 
-  def push(elem: JsonToken): Unit = stack.push(elem)
+  def topLevelMemberArray: Boolean = equal(JsonToken.START_ARRAY, JsonToken.START_OBJECT)
 
-  def topLevelMember: Boolean = stack.toList == List(JsonToken.START_OBJECT)
+  def inTopLevelMemberArray: Boolean = equal(JsonToken.START_OBJECT, JsonToken.START_ARRAY, JsonToken.START_OBJECT)
 
-  def topLevelMemberArray: Boolean = stack.toList == List(JsonToken.START_ARRAY, JsonToken.START_OBJECT)
+  def singleTopLevelObject: Boolean = equal(JsonToken.START_OBJECT, JsonToken.START_OBJECT)
 
-  def inTopLevelMemberArray: Boolean =
-    stack.toList == List(JsonToken.START_OBJECT, JsonToken.START_ARRAY, JsonToken.START_OBJECT)
-
-  def singleTopLevelObject: Boolean = stack.toList == List(JsonToken.START_OBJECT, JsonToken.START_OBJECT)
+  private def equal(elements: JsonToken*): Boolean = toSeq == elements
 }
